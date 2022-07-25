95e612e57f7fec6c534d182a4b46bfd56400888a
edgarv0508 <80708587+edgarv0508@users.noreply.github.com>
2022-07-24 19:50:25 -0500

    Item stuff

--- src/main/java/com/example/cs3773project/ItemController.java
+++ src/main/java/com/example/cs3773project/ItemController.java
@@ -1,4 +1,29 @@
 package com.example.cs3773project;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class ItemController {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    void createItem(MouseEvent event) {
+        Stage stage = (Stage) button.getScene().getWindow();
+
+    }
+
+    @FXML
+    void modifyItem(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reutrnHome(MouseEvent event) {
+
+    }
+
 }
--- src/main/java/com/example/cs3773project/Main.java
+++ src/main/java/com/example/cs3773project/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Main");
         stage.setScene(scene);
         stage.show();
     }
