f748efe2fba4158e7a07e11fbc0abf8e929f1678
edgarv0508 <80708587+edgarv0508@users.noreply.github.com>
2022-07-25 14:48:50 -0500

    Scene change for testing

--- src/main/java/com/example/cs3773project/MainController.java
+++ src/main/java/com/example/cs3773project/MainController.java
@@ -1,14 +1,42 @@
 package com.example.cs3773project;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button item, order, user;
+
+    @FXML
+    void goItems(MouseEvent event) throws IOException{
+        Stage stage = (Stage) item.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("item-view.fxml"));
+        stage.setTitle("Items");
+        stage.setScene(new Scene(root));
     }
+
+    @FXML
+    void goOrders(MouseEvent event) throws IOException {
+        Stage stage = (Stage) order.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("order-view.fxml"));
+        stage.setTitle("Orders");
+        stage.setScene(new Scene(root));
+    }
+
+    @FXML
+    void goUser(MouseEvent event) throws IOException{
+        Stage stage = (Stage) user.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("user-view.fxml"));
+        stage.setTitle("Users");
+        stage.setScene(new Scene(root));
+    }
+
 }
