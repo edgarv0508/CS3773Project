0f05eee99461d780d356b9a1e91b3fee07883f9b
DivinieBanhBao <isaacnguyen123@gmail.com>
2022-07-24 20:47:06 -0500

    Isaac Testing

--- src/main/java/com/example/cs3773project/Main.java
+++ src/main/java/com/example/cs3773project/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-//Test
+
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
