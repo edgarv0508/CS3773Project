b4d1b1dc3ff9ae5c994a8864dd787f716de58929
edgarv0508 <80708587+edgarv0508@users.noreply.github.com>
2022-07-24 20:15:04 -0500

    Scene change

--- src/main/java/com/example/cs3773project/ItemController.java
+++ src/main/java/com/example/cs3773project/ItemController.java
@@ -1,9 +1,14 @@
 package com.example.cs3773project;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ItemController {
 
@@ -11,14 +16,19 @@ public class ItemController {
     private Button button;
 
     @FXML
-    void createItem(MouseEvent event) {
+    void createItem(MouseEvent event) throws IOException {
         Stage stage = (Stage) button.getScene().getWindow();
-
+        Parent root = FXMLLoader.load(getClass().getResource("item-create.fxml"));
+        stage.setTitle("Create Item");
+        stage.setScene(new Scene(root));
     }
 
     @FXML
-    void modifyItem(MouseEvent event) {
-
+    void modifyItem(MouseEvent event) throws IOException {
+        Stage stage = (Stage) button.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("item-modify.fxml"));
+        stage.setTitle("Modify Item");
+        stage.setScene(new Scene(root));
     }
 
     @FXML
