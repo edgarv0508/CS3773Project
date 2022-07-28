90c422c37287eb086af7a026f96186ecb6331df5
zenghan0 <109928895+zenghan0@users.noreply.github.com>
2022-07-28 12:12:40 -0500

    Update HistoryOrder.java

--- src/main/java/com/example/cs3773project/HistoryOrder.java
+++ src/main/java/com/example/cs3773project/HistoryOrder.java
@@ -1,1 +1,52 @@
+ public class OrderController {
+    
+      private IOrderService orderService;
+  
+     
+      public void list(int cusid, HttpServletResponse response,int page,int rows) throws IOException {
+          response.setContentType("text/html;charset=UTF-8");
+          DatagridResult datagridResult = orderService.list(cusid,page,rows);
+          response.getWriter().print(JSON.toJSONStringWithDateFormat(datagridResult, "yyyy-MM-dd HH:mm:ss"));
+      }
+     
+      public void findById(int id,HttpServletResponse response) throws IOException {
+          response.setContentType("text/html;charset=UTF-8");
+          TCustomerOrder order = orderService.findById(id);
+          response.getWriter().print(JSON.toJSONStringWithDateFormat(order, "yyyy-MM-dd HH:mm:ss"));
+      }
+  
+  }
 
+public interface IOrderService {
+      
+      DatagridResult list(int cusid, int page, int rows);
+  
+      TCustomerOrder findById(int id);
+  
+  }
+
+public class OrderDetailsController {
+     
+      private IOrderDetailsService orderDetailsService;
+    
+      public Map getTotalPrice(int orderId){
+          Map map = new HashMap();
+          int totalMoney = orderDetailsService.getTotalPrice(orderId);
+          map.put("totalMoney",totalMoney);
+          return map;
+      }
+     
+      public DatagridResult list(int orderId, int page, int rows){
+          return orderDetailsService.selectOrderDetilsList(orderId,page,rows);
+      }
+  }
+
+
+IOrderDetailsService
+
+  public interface IOrderDetailsService {
+     
+      int getTotalPrice(int orderId);
+  
+      DatagridResult selectOrderDetilsList(int orderId, int page, int rows);
+  }
